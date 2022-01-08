0c3fa18ae45cdf9cbc953589268f3bc55f06f537
HikaruSplash <34542678+Splasheri@users.noreply.github.com>
2022-01-09 03:37:20 +0500

    qeq

--- src/components/GalleryPage.vue
+++ src/components/GalleryPage.vue
@@ -1,37 +1,44 @@
 <template>
-    <div>
-        <div class="row">
-            <div v-for="(image, index) in Images" :key="image.imageSource"
-                     class="column">
-                    <img v-bind:src="image.imageSource"
-                         v-on:click="openModal();showSlides(index + 1)"
-                         width="200px" height="200px">
-                </div>
-        </div>
-        <div id="myModal" class="modal">
-            <span class="close cursor" v-on:click="closeModal">&times;</span>
-            <div class="modal-content">
-                <div v-for="(image, index) in Images" :key="image.imageSource"
-                     class="mySlides">
-                    <div class="numbertext">{{index + 1}} / 4</div>
-                    <img v-bind:src="image.imageSource" style="width:100%">
-                </div>
-                <!-- Next/previous controls -->
-                <a class="prev" v-on:click="plusSlides(-1)">&#10094;</a>
-                <a class="next" v-on:click="plusSlides(1)">&#10095;</a>
-                <!-- Caption text -->
-                <div class="caption-container">
-                    <p id="caption"></p>
-                </div>
-                <!-- Thumbnail image controls -->
-                <div v-for="(image, index) in Images" :key="image.imageSource"
-                     class="column">
-                    <img v-bind:src="image.imageSource"
-                         v-on:click="showSlides(index + 1)"
-                         class="demo">
-                </div>
+    <div class="container">
+        <div class="image-container">
+            <div v-for="image in Images" :key="image.id"
+                class="quarter-image-container">
+                <img v-bind:src="image.imageSource" class="thumbnail"
+                     v-on:click="selectImage(image.id);toggleModal();"/>
             </div>
         </div>
+        <div v-if="isModalActive" class="modal">            
+            <div class="close-btn-container">
+                <span class="modal-btn close-btn"
+                      v-on:click="unselectImages();toggleModal()">
+                &times;
+                </span>
+            </div>
+            <div class="modal-image-container">    
+                <div class="side-btn-container">            
+                    <span v-on:click="selectPrevImage"
+                          class="modal-btn side-btn">
+                        &lt;
+                    </span>
+                </div>
+                <div class="full-image-container">
+                    <img v-bind:src="Images.find(img => img.isSelected).imageSource">
+                </div>   
+                <div class="side-btn-container">            
+                    <span v-on:click="selectNextImage"
+                          class="modal-btn side-btn">
+                        &gt;
+                    </span>
+                </div>  
+            </div>
+            <div class="modal-miniatures-container">
+                <div v-for="image in getNearImages()" :key="image.id"
+                     class="miniature">
+                    <img v-bind:src="image.imageSource"
+                         v-on:click="unselectImages();selectImage(image.id);">
+                </div>
+            </div>
+        </div>        
     </div>
 </template>
 
@@ -39,160 +46,185 @@
 export default {
     data() {
         return {
+            isModalActive: false,
             Images: [
-                {imageSource: 'https://sun9-53.userapi.com/impg/c857724/v857724948/15aa54/UHyUVwAAYg0.jpg?size=210x240&quality=96&sign=e9e03e32453dd63994b093836cfcf2f8&type=album'},
-                {imageSource: 'https://sun9-17.userapi.com/impg/c858320/v858320413/15f6a6/3M1s43N19y8.jpg?size=300x300&quality=96&sign=67f87299572cad0e87503a3e4e3aa8b7&type=album'},
-                {imageSource: 'https://sun1-13.userapi.com/impg/5tZf4FmcRstydmMc1jDHc26lwZitAeja0XWDfw/SYCZBAj8EAU.jpg?size=654x654&quality=96&sign=83b1c118f154995f702e1a0e79bb2885&type=album'}
+                {
+                    id:1, 
+                    imageSource: 'https://sun9-53.userapi.com/impg/c857724/v857724948/15aa54/UHyUVwAAYg0.jpg?size=210x240&quality=96&sign=e9e03e32453dd63994b093836cfcf2f8&type=album',
+                    isSelected: false
+                },
+                {
+                    id:2, 
+                    imageSource: 'https://sun9-17.userapi.com/impg/c858320/v858320413/15f6a6/3M1s43N19y8.jpg?size=300x300&quality=96&sign=67f87299572cad0e87503a3e4e3aa8b7&type=album',
+                    isSelected: false
+                    },
+                {   
+                    id:3, 
+                    imageSource: 'https://sun1-13.userapi.com/impg/5tZf4FmcRstydmMc1jDHc26lwZitAeja0XWDfw/SYCZBAj8EAU.jpg?size=654x654&quality=96&sign=83b1c118f154995f702e1a0e79bb2885&type=album',
+                    isSelected: false                    
+                }
             ],
             slideIndex: 1
         }
     },
     methods : {
-        openModal: function () {
-            document.getElementById("myModal").style.display = "block";
+        selectImage: function (id) {       
+          this.Images.find(img => img.id == id).isSelected = true;
         },
-        closeModal: function () {
-            document.getElementById("myModal").style.display = "none";            
+        unselectImages: function() {
+            this.Images.forEach(img => img.isSelected = false);
         },
-        plusSlides : function (n) {
-            showSlides(this.slideIndex += n);
+        toggleModal: function () {
+            this.isModalActive = !this.isModalActive
         },
-        currentSlide : function (n) {
-            showSlides(this.slideIndex = n);
+        getSelectedImage: function () {
+            var index = this.Images.findIndex(image => image.isSelected)            
+            return index < 0 ? 0 : index;
         },
-        showSlides : function (n) {            
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("demo");
-            var captionText = document.getElementById("caption");
-            if (n > slides.length) {this.slideIndex = 1}
-            if (n < 1) {this.slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[this.slideIndex-1].style.display = "block";
-            dots[this.slideIndex-1].className += " active";
-            captionText.innerHTML = dots[this.slideIndex-1].alt;
-        }   
+        getPrevImageIndex: function (imageIndex) {
+            return (imageIndex - 1 < 0 ? this.Images.length : imageIndex) - 1
+        },
+        getNextImageIndex: function (imageIndex) {
+            return imageIndex + 1 == this.Images.length ? 0 : imageIndex + 1
+        },
+        selectPrevImage: function () {
+          var index = this.getSelectedImage();
+          this.selectImageByIndex(this.getPrevImageIndex(index));
+        },
+        selectNextImage: function () {
+          var index = this.getSelectedImage();
+          this.selectImageByIndex(this.getNextImageIndex(index));
+        },
+        selectImageByIndex: function (imageIndex) {
+            this.unselectImages();
+            this.Images[imageIndex].isSelected = true;
+        },
+        getNearImages: function () {
+            var index = this.getSelectedImage();     
+            return [
+                this.Images[this.getPrevImageIndex(index)],
+                this.Images[index],
+                this.Images[this.getNextImageIndex(index)]
+            ];
+        }
     },
     name: 'GalleryPage'
 }
 </script>
 
 <style scoped>
-    .row > .column {
-        padding: 0 8px;
+    .container {
+        display: block;
     }
-    .row:after {
-        content: "";
-        display: table;
-        clear: both;
+    .image-container {
+        padding: 64px;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-around;
+    }  
+    .quarter-image-container {                       
+        width: 15%;
+        height: 15%;
+        max-width: 200px;
+        max-height: 200px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
-    /* Create four equal columns that floats next to eachother */
-    .column {
-        float: left;
-        width: 25%;
+    .quarter-image-container > img {
+        height: 100%;
+        width: 100%;
+        object-fit: contain;              
     }
-    /* The Modal (background) */
-    .modal {
-        display: none;
+    .thumbnail {
+        border: 1px solid gainsboro;
+        border-radius: 4px;
+        padding: 5px;
+    }
+    .thumbnail:hover {
+        box-shadow: 0 0 2px 1px darkcyan;
+    } 
+
+    .modal {    
         position: fixed;
-        z-index: 1;
-        padding-top: 100px;
-        left: 0;
-        top: 0;
         width: 100%;
         height: 100%;
+        z-index: 1;
+        left: 0;
+        top: 0;
         overflow: auto;
         background-color: black;
-    }
-    /* Modal Content */
-    .modal-content {
-        position: relative;
-        background-color: #fefefe;
-        margin: auto;
-        padding: 0;
-        width: 90%;
-        max-width: 500px;
-    }
-    /* The Close Button */
-    .close {
+        opacity: 1;  
+        
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+    }   
+    .modal-btn {
         color: white;
-        position: absolute;
-        top: 10px;
-        right: 25px;
-        font-size: 35px;
-        font-weight: bold;
-    }
-    .close:hover,
-    .close:focus {
-        color: #999;
-        text-decoration: none;
-    cursor: pointer;
-    }
-    /* Hide the slides by default */
-    .mySlides {
-        display: none;
-    }
-    /* Next & previous buttons */
-    .prev,
-    .next {
+        font-size: 100px;
+        font-weight: bold;        
         cursor: pointer;
-        position: absolute;
-        top: 50%;
-        width: auto;
-        padding: 16px;
-        margin-top: -50px;
-        color: white;
-        font-weight: bold;
-        font-size: 20px;
-        transition: 0.6s ease;
-        border-radius: 0 3px 3px 0;
-        user-select: none;
-        -webkit-user-select: none;
+        text-shadow: 0px 0px 5px white;
+    }
+    .modal-btn:hover {
+        color: gainsboro;
+        text-shadow: none;
+    }
+    .close-btn-container {    
+        height: 10%;    
+        width: 100%;
+        text-align: right;        
+    }
+    .close-btn {
+        margin-right: 30px;
     }
 
-    /* Position the "next button" to the right */
-    .next {
-        right: 0;
-        border-radius: 3px 0 0 3px;
+    .modal-image-container {
+        height: 70%;
+        display: flex;
+        flex-direction: row;        
+        align-items: center;
+        justify-content: center;
     }
-    /* On hover, add a black background color with a little bit see-through */
-    .prev:hover,
-    .next:hover {
-        background-color: rgba(0, 0, 0, 0.8);
+    .side-btn-container {        
+        width: 10%;
     }
-    /* Number text (1/3 etc) */
-    .numbertext {
-        color: #f2f2f2;
-        font-size: 12px;
-        padding: 8px 12px;
-        position: absolute;
-        top: 0;
+    .side-btn {
+        width: auto;
     }
-    /* Caption text */
-    .caption-container {
-        text-align: center;
-        background-color: black;
-        padding: 2px 16px;
-        color: white;
+    .full-image-container {        
+        display: flex;
+        align-content: center;
+        justify-content: center;
+        width: 60%;        
+        max-height: 100%;
     }
-    img.demo {
-        opacity: 0.6;
-        width: 200px;
-        height: 200px
+    .full-image-container > img { 
+        object-fit: contain;
+        max-width: 100%;
+        max-height: 100%;
     }
-    .active,
-    .demo:hover {
-        opacity: 1;
+
+    .modal-miniatures-container {
+        height: 20%;
+        display: flex;
+        align-content: center;
+        justify-content: center;        
     }
-    img.hover-shadow {
-        transition: 0.3s;
+    .miniature {
+        max-height: 100%;
+        max-width: 30%;
+        padding: 10px 10px 0px 10px;        
     }
-    .hover-shadow:hover {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    .miniature > img {
+        object-fit: contain;
+        max-width: 100%;
+        max-height: 100%;
+    }
+    .miniature > img:hover {
+        opacity: 0.5;
+        cursor: pointer;
     }
 </style>
